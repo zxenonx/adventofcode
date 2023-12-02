cfad1f2f98d32142af6d05325d4e09c3ccaead7a
xzenon <xenon.nemrod@gmail.com>
2023-12-02 19:25:59 +0100

    Solve day 1 part 1 challenge

---
+++ aoc2023/day_1/part_1_trebuchet.py
@@ -0,0 +1,16 @@
+'''
+Day 1 of AOC - trebuchet
+'''
+import re
+
+with open("input.txt") as input:
+    content = input.readlines()
+    content = [line.strip() for line in content]
+
+    total_calibration = 0
+    for line in content:
+        only_nums = re.sub("\D+", "",line)
+
+        total_calibration += int(only_nums[0]+only_nums[-1])
+
+    print(total_calibration)    
